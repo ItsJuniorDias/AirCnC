5e9020e1293f73f4c9a2a87f2c9a4c2969981982
Junior <juniordias_@live.com>
2019-10-02 21:35:05 -0300

    frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnc"/>
+    </div>
+
+  );
+}
+
+export default App;
