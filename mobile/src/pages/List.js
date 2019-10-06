e47f58a63e998dc71d9be4b84dc88212afb34643
Alexandre <juniordias_@live.com>
2019-10-06 13:57:35 -0300

    Spots mobile

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import { View, SafeAreaView, ScrollView,StyleSheet,Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List(){
-    return <View/>
+   const [techs, setTechs] = useState([]);
+
+   useEffect(() => {
+     AsyncStorage.getItem('techs').then(storagedTechs =>{
+         const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+         setTechs(techsArray);
+     })
+   }, []);
+
+    return (
+    <SafeAreaView style={StyleSheet.container}>
+       <Image style={styles.logo} source={logo}/>
+ 
+       <ScrollView>
+          {techs.map(tech => <SpotList key={tech} tech={tech}/>)}
+       </ ScrollView>
+    </SafeAreaView> 
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+
+    logo: {
+       height:32,
+       resizeMode: "contain",
+       alignSelf:'center',
+       marginTop: 10,
+
+    },
+})
